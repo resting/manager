db4f8d7b7719d284e28c04c780a86bd927159c37
resting <roy.wong.80@gmail.com>
2017-03-19 13:00:59 +0800

    Fix undefined error.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedBack, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
   onRowPress() {
-    // Actions.employeeCreate();
+    Actions.employeeCreate({ employee: this.props.employee });
   }
 
   render() {
     const { name } = this.props.employee;
 
     return (
-      <TouchableWithoutFeedBack onPress={this.onRowPress.bind(this)}>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
         <View>
           <CardSection>
             <Text style={styles.titleStyle}>
@@ -20,7 +20,7 @@ class ListItem extends Component {
             </Text>
           </CardSection>
         </View>
-      </TouchableWithoutFeedBack>
+      </TouchableWithoutFeedback>
     );
   }
 }
